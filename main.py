742f67e3c1949454ec2b1b9b5bf19b0c182ec067
Pavel Filippenko <rubikpf2002@mail.ru>
2022-04-18 16:29:33 +0300

    Add mutation of single files

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import Mutator
 
 def main():
 
-    DIR_NAME = "./testcases/"
-
-    # переменная хранит названия файлов в testcases
-    dirfiles = os.listdir(DIR_NAME) 
-
-    for file in dirfiles:
-        FILE_NAME = file
-
+    # можно мутировать заданный файл
+    if len(argv) == 2:
+        FILE_NAME = argv[1]
+        
         # открываем файл и проверям успешность открытия
-        fd_src = open(DIR_NAME + FILE_NAME, 'r')
+        fd_src = open(FILE_NAME, 'r')
         if (fd_src == -1):
             print("Error open")
             exit(1)
@@ -27,6 +23,30 @@ def main():
         Mutator.Parser(fd_src, fd_dst)
         fd_dst.close()
         fd_src.close()
+    # или мутировать все файлы из указанной дериктории (дефолтный режим работы)
+    else:
+        DIR_NAME = "./testcases/"
+
+        # переменная хранит названия файлов в testcases
+        dirfiles = os.listdir(DIR_NAME) 
+
+        for file in dirfiles:
+            FILE_NAME = file
+
+            # открываем файл и проверям успешность открытия
+            fd_src = open(DIR_NAME + FILE_NAME, 'r')
+            if (fd_src == -1):
+                print("Error open")
+                exit(1)
+
+            fd_dst = open("mutated_" + FILE_NAME, 'w')
+            if (fd_dst == -1):
+                print("Error open")
+                exit(1)
+
+            Mutator.Parser(fd_src, fd_dst)
+            fd_dst.close()
+            fd_src.close()
 
 if __name__ == '__main__':
     main()
